c32636504ac89f64754c992d796259fb67eae22d
Pravin Kumar <2030111@sliet.ac.in>
2022-05-28 10:32:17 +0530

    Testing launch

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import Cryptocontext from './Cryptocontext';
 import 'react-alice-carousel/lib/alice-carousel.css';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
+import {BrowserRouter} from "react-router-dom"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
