95e878ee4f0f05e192d0c60ccfaa36b802e58fdc
Pravin Kumar <2030111@sliet.ac.in>
2022-05-28 10:52:45 +0530

    Testing final

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ import {BrowserRouter} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+ 
   <React.StrictMode>
    <Cryptocontext> <App />
    </Cryptocontext>
   </React.StrictMode>
-  </BrowserRouter>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
