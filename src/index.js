2b3c4202c4942a2e31dbef1ec04fcb4ea72a52b6
Pravin Kumar <2030111@sliet.ac.in>
2022-05-28 10:28:54 +0530

    Testing launch

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import Cryptocontext from './Cryptocontext';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
    <Cryptocontext> <App />
    </Cryptocontext>
   </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
